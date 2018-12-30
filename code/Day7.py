8b270fc006361e8e4f979d2e01293edd52cb5d55
shubhampy <shubhamgarg.py@gmail.com>
2018-12-31 00:03:59 +0530

    check if a string contains any special character

--- code/Day7.py
+++ code/Day7.py
@@ -1,5 +1,11 @@
-####################################################################################
-#                                   Day-7                                          #
-####################################################################################
-#                                                                                  #
-####################################################################################
+#- Program to check if a string contains any special character
+
+import re
+
+any_string = 'hurray! @we are % here  on day7$ *xd'
+
+checker= re.findall('[^\w\s]',any_string)         #special char. @_!#$%^&*()<>?/\|}{~:]
+if checker:
+    print('String are contains special character like {}'.format(checker))
+else:
+    print('not any special character')
