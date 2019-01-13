4e0278d364d3d31e0f1208a9457c21a1ec9fe388
Shubham Garg <39944682+ShubhamPy@users.noreply.github.com>
2019-01-13 21:37:42 +0530

    add univProgs

---
+++ javaExercises/University/assignments/src/addUserNum.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class addUserNum {
+    public static void main(String[] args) {
+        int firstNumber;
+        int secondNumber;
+        int result;
+
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Enter First Number:");
+        firstNumber = userInput.nextInt();
+        System.out.println("Enter Second Number:");
+        secondNumber = userInput.nextInt();
+
+        result = firstNumber + secondNumber;
+        System.out.println("Addition of these two number is:"+result);
+
+    }
+}
