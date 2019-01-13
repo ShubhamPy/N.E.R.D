53c671bd43d3d0f90d70ce15d60c5be7c5bee699
Shubham Garg <39944682+ShubhamPy@users.noreply.github.com>
2019-01-13 20:28:19 +0530

    day0 with java

---
+++ javaExercises/Basics/HelloWorld/src/HelloUser.java
@@ -0,0 +1,8 @@
+//java beginner
+//start java with classic approach with printing famous hello world message
+
+public class HelloUser {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
