c88d4e51584765505529ed62df52ae8827eb0945
Shubham Garg <39944682+ShubhamPy@users.noreply.github.com>
2019-01-13 20:59:02 +0530

    Update HelloUser.java

--- javaExercises/Basics/HelloWorld/src/HelloUser.java
+++ javaExercises/Basics/HelloWorld/src/HelloUser.java
@@ -1,5 +1,5 @@
 //java beginner
-//start java with classic approach with printing famous hello world message
+//start java with classic approach of printing famous hello world message
 
 public class HelloUser {
     public static void main(String[] args) {
