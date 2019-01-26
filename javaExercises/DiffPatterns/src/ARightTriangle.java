488ae594ee88d4458c3770318e947f638b614082
Shubham Garg <39944682+ShubhamPy@users.noreply.github.com>
2019-01-27 01:12:16 +0530

    Update and rename RightTriangle.java to ARightTriangle.java

--- javaExercises/DiffPatterns/src/ARightTriangle.java
+++ javaExercises/DiffPatterns/src/ARightTriangle.java
@@ -39,7 +39,7 @@ Output produced here:
 now again j value increments,and j becomes j=2 but now condition is false so it come out from loop and print nextline function for 1st loop.
 and so on it print the pattern upto i=5 then it come out from 1st loop also.
 */
-public class RightTriangle {
+public class ARightTriangle {
     public static void main(String[] args) {
         for(int i=1;i<=5;i++){
             for(int j=0;j<i;j++){
