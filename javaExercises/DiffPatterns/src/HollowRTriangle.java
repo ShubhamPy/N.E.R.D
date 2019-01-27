edc02f21ce0b29d3c85ea34959914597767beca7
Shubham Garg <39944682+ShubhamPy@users.noreply.github.com>
2019-01-27 17:25:28 +0530

    Create HollowRTriangle.java

---
+++ javaExercises/DiffPatterns/src/HollowRTriangle.java
@@ -0,0 +1,25 @@
+/*
+Hollow Right Triangle Star Pattern
+
+*
+**
+* *
+*  *
+*****
+    
+*/
+
+
+public class HollowRTriangle {
+    public static void main(String[] args){
+        for(int i=1;i<=5;i++){
+            for(int j=0;j<i;j++){
+                if(i==1||i==5||j==0||j==i-1){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }System.out.println();
+        }
+    }
+}
