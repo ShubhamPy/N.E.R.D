bfb84872c6eb118e1b51a3bec50af4ffdbd3133c
Shubham Garg <39944682+ShubhamPy@users.noreply.github.com>
2019-01-29 18:54:21 +0530

    Create MirrorRTriangle.java

---
+++ javaExercises/DiffPatterns/src/MirrorRTriangle.java
@@ -0,0 +1,24 @@
+/*
+Mirror Right Triangle Star Pattern
+
+    *
+   **
+  ***
+ ****
+*****
+
+*/
+public class MirrorRTriangle{
+
+     public static void main(String []args){
+         for (int i=1;i<=5;i++){
+             for(int j=i;j<=5;j++){
+                 System.out.print(" ");
+             }
+             for(int j=1;j<=i;j++){
+                 System.out.print("*");
+             }
+             System.out.println();
+         }
+     }
+}
