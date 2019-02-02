9c330bbd71f5b664dec71d454a63bea01d926453
Shubham Garg <39944682+ShubhamPy@users.noreply.github.com>
2019-02-02 23:43:46 +0530

    Create HollinvMirrRightTriangle.java

---
+++ javaExercises/DiffPatterns/src/HollinvMirrRightTriangle.java
@@ -0,0 +1,31 @@
+/*
+Hollow Inverted Mirror Right Triangle Star Pattern
+
+*****
+ *  *
+  * *
+   **
+    *
+
+*/
+
+public class HollinvMirrRightTriangle{
+
+     public static void main(String []args){
+         for(int i=1;i<=5;i++){
+             for(int j=1;j<i;j++){
+                 System.out.print(" ");
+             }
+             for(int j=5;j>=i;j--){
+                 if(i==j||i==1||j==5){
+                     System.out.print("*");
+                     
+                 }else{
+                     System.out.print(" ");
+                 }
+                 
+             }
+             System.out.println();
+         }
+     }
+}
