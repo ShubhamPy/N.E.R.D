149a614f3882a227e4146733bc86f763b23476a1
Shubham Garg <39944682+ShubhamPy@users.noreply.github.com>
2019-02-02 23:40:21 +0530

    Create InvMirrRightTriangle.java

---
+++ javaExercises/DiffPatterns/src/InvMirrRightTriangle.java
@@ -0,0 +1,25 @@
+/*
+Inverted Mirror Right Triangle Star Pattern
+
+*****
+ ****
+  ***
+   **
+    *
+
+*/
+
+public class InvMirrRightTriangle{
+
+     public static void main(String []args){
+         for(int i=1;i<=5;i++){
+             for(int j=1;j<i;j++){
+                 System.out.print(" ");
+             }
+             for(int j=5;j>=i;j--){
+                 System.out.print("*");
+             }
+             System.out.println();
+         }
+     }
+}
