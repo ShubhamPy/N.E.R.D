d42e30c464ab6e260354c84633e36f3106c8cc4a
Shubham Garg <39944682+ShubhamPy@users.noreply.github.com>
2019-02-03 14:37:05 +0530

    Update InvPyramid.java

--- javaExercises/DiffPatterns/src/InvPyramid.java
+++ javaExercises/DiffPatterns/src/InvPyramid.java
@@ -17,7 +17,7 @@ public class InvPyramid{
              for(int j=1;j<i;j++){
                  System.out.print(" ");
              }
-             for(int j=10;j>(2*i-1);j--){
+             for(int j=1;j<=(10-(2*i-1));j++){
                  System.out.print("*");
              }
              System.out.println();
