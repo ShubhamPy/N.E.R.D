a2f682793e64878e993f8a2309e02306b35f65e4
Shubham Garg <39944682+ShubhamPy@users.noreply.github.com>
2019-02-03 14:20:03 +0530

    Create InvPyramid.java

---
+++ javaExercises/DiffPatterns/src/InvPyramid.java
@@ -0,0 +1,26 @@
+/*
+Inverted Pyramid Star Pattern
+
+*********
+ *******
+  *****
+   ***
+    *
+
+
+*/
+
+public class InvPyramid{
+
+     public static void main(String []args){
+         for(int i=1;i<=5;i++){
+             for(int j=1;j<i;j++){
+                 System.out.print(" ");
+             }
+             for(int j=10;j>(2*i-1);j--){
+                 System.out.print("*");
+             }
+             System.out.println();
+         }
+     }
+}
